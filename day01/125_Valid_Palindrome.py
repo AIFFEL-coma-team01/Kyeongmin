453a73bbe019befdf8a04210557b869e50872000
wonderkyeom <wonderkyum21@naver.com>
2021-01-13 15:27:33 +0900

    day01

--- day01/125_Valid_Palindrome.py
+++ day01/125_Valid_Palindrome.py
@@ -3,7 +3,8 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # reverse 사용하면 되지않나?
-        s = re.sub('[-=+,#/\?:^$.@*_\{}"※~;&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', s) 
+        # s = re.sub('[-=+,#/\?:^$.@*_\{}"※~;&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', s)
+	s = re.sub('[^a-z0-9]', '', s) # 이렇게 푸는게 더 좋은거 같음.
         s = s.replace(" ", "")
         s = s.lower()
         reverse = ''.join(reversed(s))
