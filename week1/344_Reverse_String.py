3c2b9cfaf8dd4a7e2af36e1b222cbcd37114c826
wonderkyeom <wonderkyum21@naver.com>
2021-01-20 12:12:41 +0900

    week1

---
+++ week1/344_Reverse_String.py
@@ -0,0 +1,10 @@
+# 344
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        print(s.reverse())
+
+## 이딴식으로 푸는게 맞는거임??
