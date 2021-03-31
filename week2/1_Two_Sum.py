dc1f31033168a2a37a3ce56eeecaa71561941ba2
wonderkyeom <wonderkyum21@naver.com>
2021-03-31 21:14:25 +0900

    week2

--- week2/1_Two_Sum.py
+++ week2/1_Two_Sum.py
@@ -6,3 +6,24 @@ class Solution(object):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+
+
+##########################################################################
+'''
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # for i in range(len(nums)):
+        #     if (target - nums[i]) in nums:
+        #         return [i, nums.index(target - nums[i])] ## 중복인 경우를 필터할 수 없음.
+        
+        for i in range(len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if (target - nums[i]) == nums[j]:
+                    return [i, j]
+'''
